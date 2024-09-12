94324962b23daac3fec920547c365a2f87283a8d
akinkuti <91487111+akinkuti@users.noreply.github.com>
2024-09-12 01:02:24 +0100

    Update variables-modules.tf

--- variables-modules.tf
+++ variables-modules.tf
@@ -6,7 +6,9 @@ resource "aws_instance" "ec2" {
   ami = var.ami
   instance_type = var.instance_type
 }
-
+output "instance_id" {
+  value =aws_instance.afka-ec2.id
+}
 variable "region" {}
 variable "ami" {}
 variable "instance_type" {}
